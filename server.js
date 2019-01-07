03627bf7c11cf8f830f1519f50579c86dd2f5922
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-01-07 17:11:12 +0530

    Added a Schema for Employees

--- server.js
+++ server.js
@@ -5,16 +5,50 @@ var app = express();
 
 var buildScheme = require("graphql").buildSchema;
 
+
+var EmplArray = [{
+    id: 0,
+    name: "Mayank",
+    designation: "Developer"
+}, {
+    id: 1,
+    name: "Meha",
+    designation: "Developer"
+}, {
+    id: 2,
+    name: "Anshul",
+    designation: "Business"
+}]
+
 var scheme = buildScheme(`
     type Query {
         message: String
+        employee(id: Int): Employee
+        employees(designation: String, id: Int): [Employee]
+    }
+
+    type Employee {
+        id: Int,
+        name: String
+        designation: String
     }
 `);
+
+function getEmployee(args) {
+    var id = args.id;
+    return EmplArray.filter(emp => emp.id == id)[0];
+}
+
+function getEmployees(args) {
+    return EmplArray.filter(emp => (emp.designation == args.designation && emp.id == args.id));
+}
 
 // Defining a resolver:
 
 var root = {
-    message: () => "Hello World"
+    message: () => "Hello World",
+    employee: getEmployee,
+    employees: getEmployees
 }
 
 app.use("/graphql", express_graphql({
